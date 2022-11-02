91203dcd68244d6f66c50c712e8a1f4fb2c0cee2
maede kaboli <kabolimaede@gmail.com>
2022-11-02 16:22:03 +0330

    extra css removed

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -60,30 +60,6 @@ export default {
   width: 100%;
   height: 100%;
 }
-.video-js .vjs-modal-dialog {
-  border-radius: 16px;
-}
-.vjs-poster {
-  background-size: cover;
-  border-radius: 16px;
-}
-.video-js {
-  width: 100%;
-  background: rgba(220, 221, 222, 0.33);
-  border-radius: 16px;
-  height: 417px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.video-js .vjs-big-play-button {
-  position: relative;
-  width: 82px !important;
-  height: 82px !important;
-  border: 1px solid #ffdd00 !important;
-  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25)) !important;
-  border-radius: 50% !important;
-}
 .introduction {
   padding: 48px 71px 55px 71px;
   border-radius: 34px 34px 0 0;
@@ -108,25 +84,7 @@ export default {
   margin-top: 24px;
   color: #676767;
 }
-.vjs-play-control .vjs-icon-placeholder:before,
-.video-js .vjs-big-play-button .vjs-icon-placeholder:before {
-  content: "";
-  background-image: url("../assets/images/play.svg") !important;
-  background-position: center;
-  background-repeat: no-repeat;
-}
 @media only screen and (max-width: 426px) {
-  .vjs-play-control .vjs-icon-placeholder:before,
-  .video-js .vjs-big-play-button .vjs-icon-placeholder:before {
-    background-size: 40%;
-  }
-  .video-js .vjs-big-play-button {
-    width: 48px !important;
-    height: 48px !important;
-  }
-  .video-js {
-    height: 154px;
-  }
   .introduction {
     padding: 16px 16px 24px 16px;
     margin-top: -38px;
